4274da53c22f7c19748976f303ce15ff23c591ad
Wesley <weslley182@gmail.com>
2016-09-07 22:33:10 -0300

    Cadastro de usuários

---
+++ Financeiro/Controllers/UsuarioController.cs
@@ -0,0 +1,44 @@
+﻿using Financeiro.Dao;
+using Financeiro.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Financeiro.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        public ActionResult Form()
+        {
+            return View();
+        }
+
+        private UsuarioDao usuarioDao;
+
+        public UsuarioController(UsuarioDao usuarioDao)
+        {
+            this.usuarioDao = usuarioDao;
+        }
+
+        public ActionResult Adiciona(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                usuarioDao.Adiciona(usuario);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View("Form", usuario);
+            }
+        }
+
+        public ActionResult Index()
+        {
+            return View(usuarioDao.Lista());
+        }
+    }
+}
---
+++ Financeiro/Dao/UsuarioDao.cs
@@ -0,0 +1,28 @@
+﻿using Financeiro.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Financeiro.Dao
+{
+    public class UsuarioDao
+    {
+        private FinancasContext context;
+
+        public UsuarioDao(FinancasContext context)
+        {
+            this.context = context;
+        }
+
+        public void Adiciona(Usuario usuario)
+        {
+            context.Usuarios.Add(usuario);
+            context.SaveChanges();
+        }
+        public IList<Usuario> Lista()
+        {
+            return context.Usuarios.ToList();
+        }
+    }
+}
