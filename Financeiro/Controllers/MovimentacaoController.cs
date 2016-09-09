6f543243a6179ddc5e17e7a80b42b44be751facf
Wesley <weslley182@gmail.com>
2016-09-08 23:03:26 -0300

    Movimentações

---
+++ Financeiro/Controllers/MovimentacaoController.cs
@@ -0,0 +1,47 @@
+﻿using Financeiro.Dao;
+using Financeiro.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Financeiro.Controllers
+{
+    public class MovimentacaoController : Controller
+    {
+        private MovimentacaoDao movimentacaoDao;
+        private UsuarioDao usuarioDao;
+        public MovimentacaoController(MovimentacaoDao movimentacaoDAO, UsuarioDao usuarioDAO)
+        {
+            this.movimentacaoDao = movimentacaoDAO;
+            this.usuarioDao = usuarioDAO;
+        }
+
+        // GET: Movimentacao
+        public ActionResult Index()
+        {
+            return View(movimentacaoDao.Lista());
+        }
+
+        public ActionResult Form()
+        {
+            ViewBag.Usuarios = usuarioDao.Lista();
+            return View();
+        }
+
+        public ActionResult Adiciona(Movimentacao movimentacao)
+        {
+            if (ModelState.IsValid)
+            {
+                movimentacaoDao.Adiciona(movimentacao);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.Usuarios = usuarioDao.Lista();
+                return View("Form");
+            }
+        }
+    }
+}
---
+++ Financeiro/Dao/MovimentacaoDao.cs
@@ -0,0 +1,26 @@
+﻿using Financeiro.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Financeiro.Dao
+{
+    public class MovimentacaoDao
+    {
+        private FinancasContext context;
+        public MovimentacaoDao(FinancasContext context)
+        {
+            this.context = context;
+        }
+        public void Adiciona(Movimentacao movimentacao)
+        {
+            context.Movimentacoes.Add(movimentacao);
+            context.SaveChanges();
+        }
+        public IList<Movimentacao> Lista()
+        {
+            return context.Movimentacoes.ToList();
+        }
+    }
+}
